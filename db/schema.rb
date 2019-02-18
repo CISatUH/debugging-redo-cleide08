c5cce8847ce33744b9cfd3048790ee838b48a313
cleide08 <cleidy.08@hotmail.com>
2019-02-18 15:47:04 -0600

    completed hours for task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_215746) do
+ActiveRecord::Schema.define(version: 2019_02_18_214302) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_215746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "complete"
+    t.integer "hours"
     t.index ["job_id"], name: "index_tasks_on_job_id"
   end
 
